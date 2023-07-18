bf7c40bd6269078b9fe8fb74f99e7d056828c79c
donneyluck <donney.luck@gmail.com>
2023-07-18 13:27:35 +0800

    update ..

--- dot_config/i3/executable_wallpic.sh
+++ dot_config/i3/executable_wallpic.sh
@@ -4,7 +4,8 @@ pidfile=$CUR_DIR/wallpic.pid
 
 if [ -f "$pidfile" ] && kill -0 `cat $pidfile` 2>/dev/null; then
 	echo still running
-    feh --randomize --bg-scale ~/Pictures/wallpaper/*
+    #feh --randomize --bg-scale ~/Pictures/wallpaper/*
+    dwall -s bitday
     exit 1
 fi
 echo $$ > $pidfile
@@ -12,6 +13,7 @@ echo $$ > $pidfile
 while true; do
     #find ~/Pictures -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |
     #    shuf -n1 -z | xargs -0 feh --bg-scale
-    feh --randomize --bg-scale ~/Pictures/wallpaper/*
+    #feh --randomize --bg-scale ~/Pictures/wallpaper/*
+    dwall -s bitday
     sleep 15m
 done
--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,10 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-source /usr/share/autojump/autojump.fish
+if test -f /home/donney/.autojump/share/autojump/autojump.fish; . /home/donney/.autojump/share/autojump/autojump.fish; end
+
 export EDITOR=vim
-export http_proxy='127.0.0.1:7890'
-export https_proxy='127.0.0.1:7890'
+export HTTP_PROXY='127.0.0.1:7890'
+export HTTPS_PROXY='127.0.0.1:7890'
 export ALL_PROXY='socks5://127.0.0.1:7891'
 export TERM=xterm;
 
@@ -20,11 +21,15 @@ alias genpasswd "strings /dev/urandom | grep -o '[[:alnum:]]' | head -n 30 | tr 
 alias busy "cat /dev/urandom | hexdump -C | grep 'ca fe'"
 
 for host in (awk '/^Host/{if ($2!="*") print $2}' ~/.ssh/config)
-  alias $host="ssh $host" 
+  alias $host="ssh $host"
 end
 
-function open -d "open dir use nautilus"
-   nohup nautilus -w $argv[1] > /dev/null 2>&1 & 
+#function open -d "open dir use nautilus"
+#   nohup nautilus -w $argv[1] > /dev/null 2>&1 &
+#end
+
+function open -d "open dir use thunar"
+   thunar $argv[1] & 
 end
 
 function proxy_on -d "open proxy"
@@ -35,9 +40,9 @@ function proxy_on -d "open proxy"
 end
 
 function proxy_off -d "close proxy"
-  unset http_proxy 
-  unset https_proxy 
-  unset ALL_PROXY 
+  unset http_proxy
+  unset https_proxy
+  unset ALL_PROXY
   echo -e "proxy off"
 end
 
@@ -49,27 +54,33 @@ function makescript
   history | head -1 > $argv[1]
 end
 
-function sbs 
+function sbs
   du -b --max-depth 1 | sort -nr | perl -pe 's{([0-9]+)}{sprintf "%.1f%s", $1>=2**30? ($1/2**30, "G"): $1>=2**20? ($1/2**20, "M"): $1>=2**10? ($1/2**10, "K"): ($1, "")}e'
 end
 
 function mcd -d "mkdir and enter"
   mkdir -p $argv[1]
-  cd $argv[1] 
+  cd $argv[1]
 end
 
 function cll -d "cd and ll"
-      cd $argv[1] 
+      cd $argv[1]
       ll
 end
 
-function backup 
-  cp $argv[1]{,.bak} 
+function backup
+  cp $argv[1]{,.bak}
 end
 
 function sshlist
   for host in (awk '/^Host/{if ($2!="*") print $2}' ~/.ssh/config)
-     echo $host 
+    #set_color red
+    set_color green
+    echo -n $host
+    set_color normal
+    rg $host ~/.ssh/config -A2 | awk '/^\s*HostName/{if ($2 != "*") printf " -> IP: %s",$2}'
+    rg $host ~/.ssh/config -A2 | awk '/^\s*IdentityFile/{if ($2 != "*") printf " -> ID_FILE: %s",$2}'
+    echo ""
   end
 end
 
@@ -80,20 +91,22 @@ function wtf -d "Print which and --version output for the given command"
     end
 end
 
-function ranger-cd                                                               
-  set tempfile '/tmp/chosendir'                                                  
-  /usr/bin/ranger --choosedir=$tempfile (pwd)                                    
+function ranger-cd
+  set tempfile '/tmp/chosendir'
+  /usr/bin/ranger --choosedir=$tempfile (pwd)
 
-  if test -f $tempfile                                                           
-      if [ (cat $tempfile) != (pwd) ]                                            
-        cd (cat $tempfile)                                                       
-      end                                                                        
-  end                                                                            
+  if test -f $tempfile
+      if [ (cat $tempfile) != (pwd) ]
+        cd (cat $tempfile)
+      end
+  end
 
-  rm -f $tempfile                                                                
+  rm -f $tempfile
 
-end                                                                              
-
-function fish_user_key_bindings                                                  
-    bind \co 'ranger-cd ; fish_prompt'                                           
 end
+
+function fish_user_key_bindings
+    bind \co 'ranger-cd ; fish_prompt'
+end
+
+thefuck --alias | source
